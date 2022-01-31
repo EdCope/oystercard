aa89ba88bcb6606ad2075022bdc5a59147f83c73
Charlotte Buchan <cebuc@talktalk.net>
2022-01-31 18:00:19 +0000

    set methods for the user tapping in and out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -6,7 +6,7 @@ attr_reader :balance
 # attr_reader takes the class instance variable and applies it to all objects created.
   def initialize
     @balance = 0
-
+    @in_use = false 
   end
 
   def top_up(money)
@@ -17,5 +17,13 @@ attr_reader :balance
   def deduct(money)
     @balance -= money
   end
-
+  def touch_in
+  @in_use = true
+  end
+  def in_journey?
+    @in_use
+  end
+  def touch_out
+  @in_use = false
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -20,5 +20,19 @@ describe Oystercard do
       expect{ subject.top_up(1) }.to raise_error "Card limit #{max} exceeded"
     end
   end
-
+  describe '#deduct' do
+  it "shoud deduct money from the card" do 
+    subject.top_up(20)
+    expect{subject.deduct(2)}.to change(subject, :balance).by -2
+    end 
+  end
+  describe 'touching in process/out process' do
+    it "at the start when not on a journey" do 
+      expect(subject).not_to be_in_journey
+    end
+  it "shows in journey when user touches in" do
+    subject.touch_in 
+    expect(subject).to be_in_journey
+    end
+  end 
 end
