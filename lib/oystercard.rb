d086d21723cd5c6cbfc6219c49b04272cdc1f0f3
Charlotte Buchan <cebuc@talktalk.net>
2022-01-31 15:49:08 +0000

    updated oystercard.rb and oystercard_spec.rb

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,5 +1,10 @@
 class Oystercard
+attr_reader :balance
+# attr_reader takes the class instance variable and applies it to all objects created.
+  def initialize
+    @balance = 0
+
+  end
 
 
-    
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,17 @@
 require_relative '../lib/oystercard.rb'
 
 describe Oystercard do
+describe '#balence' do 
+  it "Respond to the method balance" do 
+    expect(subject).to respond_to(:balance)
+  end  
+  it "Should have a balance of 0" do
+    expect(subject.balance).to eq(0) 
+  end
+end
 
+
+    # In order touse public transport 
+# As a customer
+# I want money on my card
 end
